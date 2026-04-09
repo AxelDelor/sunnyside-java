45f676a06572a7f6194ec0fab94b957db22fd177
axel delor <axeldelor@gmail.com>
2026-04-09 15:32:05 +0200

    generation de token avec JWT

---
+++ src/main/java/com/axeld/sunnyside2/filter/JwtAuthFilter.java
@@ -0,0 +1,34 @@
+package com.axeld.sunnyside2.filter;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.axeld.sunnyside2.service.JwtService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+  private final JwtService jwtService;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    String authHeader = request.getHeader("Authorization");
+
+    if (authHeader != null && authHeader.startsWith("Bearer")) {
+      String token = authHeader.substring(7);
+    }
+  }
+
+
+}
